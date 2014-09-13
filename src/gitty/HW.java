8e33de7b49dfbdd2fcb19d9a4d58ae47bf348aa7
voxelschool <phoco@phoco.info>
2014-09-13 15:09:00 -0400

    Update HW.java

--- src/gitty/HW.java
+++ src/gitty/HW.java
@@ -3,6 +3,6 @@ package gitty;
 public class HW {
 
 	public HW() {
-		System.out.println("****hello world-small-change");
+		System.out.println("****hello world-small-change"); //a comment. can we pull this?
 	}
 }
